2de98f1fc065dfb77fb21072b37f0300996a3018
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2020-07-07 22:54:57 +0500

    create counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
-function App() {
-  return (
-    <h1>hello world</h1>
-  );
+import React, {useState} from 'react'
+import './App.css'
+function App(){
+  let [count,setCount] = useState(0);
+  let [day,setDay] = useState(true)
+  const handler=() =>{
+    setCount(++count)
+    setDay(false)
+  }
+  return(
+    <React.Fragment>
+      <div className={`${day ? 'day': 'night'}`}>
+        <h1>hello</h1>
+        <h1>{count}</h1><br/>
+        <button onClick={handler}>clickme</button>
+      </div>
+      
+    </React.Fragment>
+    
+  )
 }
-
 export default App;
