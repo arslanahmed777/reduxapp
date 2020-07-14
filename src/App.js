e8c2d89b2b06ca54a1b6852e88f37fb85e8a4173
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2020-07-14 22:52:06 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, {useState} from 'react'
-import './App.css'
+import React from 'react'
+i
+
+
+
 function App(){
-  let [count,setCount] = useState(0);
-  let [day,setDay] = useState(true)
-  const handler=() =>{
-    setCount(++count)
-    setDay(false)
-  }
+  
   return(
     <React.Fragment>
-      <div className={`${day ? 'day': 'night'}`}>
-        <h1>hello</h1>
-        <h1>{count}</h1><br/>
-        <button onClick={handler}>clickme</button>
-      </div>
-      
+      <h1>hello</h1>
     </React.Fragment>
     
+    
+      
   )
 }
 export default App;
